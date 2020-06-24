6b514e47c640167e08e3eb280994b6e4f701f912
Andrii Savchenko <andrsvchnko@gmail.com>
2020-06-24 14:32:20 +0300

    Comment variables to be changed on production

--- api.js
+++ api.js
@@ -1,5 +1,6 @@
 const CONVABOUT_PROD_API_URL = "https://convabout.herokuapp.com/api/v1/";
 const CONVABOUT_DEV_API_URL = "http://127.0.0.1:8000/api/v1/";
+// IMPORTANT: switch manually on production
 const ENV = "development";
 export const API_URL =
   ENV === "production" ? CONVABOUT_PROD_API_URL : CONVABOUT_DEV_API_URL;
