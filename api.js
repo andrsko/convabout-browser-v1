dcbc5f45987438603eb5ab44c16497fbdd9a2e5e
Andrii Savchenko <andrsvchnko@gmail.com>
2020-09-08 17:48:37 +0300

    Change api address to subdomain

--- api.js
+++ api.js
@@ -1,4 +1,4 @@
-const CONVABOUT_PROD_API_URL = "https://convabout.herokuapp.com/api/v1/";
+const CONVABOUT_PROD_API_URL = "http://api.convabout.com/api/v1/";
 const CONVABOUT_DEV_API_URL = "http://127.0.0.1:8000/api/v1/";
 // IMPORTANT: switch manually on production
 const ENV = "development";
